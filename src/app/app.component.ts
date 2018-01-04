0480bece78174f70f2a5b6e6f9ef7db4bbc8d45d
bersling <bersling@gmail.com>
2018-01-04 20:55:46 +0100

    fix lint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    private content: string = '...this will be replaced...';
-    
-    compileStringToAngularAndInsert(template: string) {
-        this.content = template;
-    }
+  content = '...this will be replaced...';
+
+  compileStringToAngularAndInsert(template: string) {
+    this.content = template;
+  }
 }
